1972136534cb2a1cb9821829dc5b5f82fd845782
Jon Mulligan <jonmulligan@me.com>
2019-07-18 15:17:14 +0900

    Basic Webpack TS Config

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class App extends React.Component<{}, {}> {
+  render() {
+    return <p>Hello World!</p>
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+
+  entry: {
+    app: './src/app.tsx'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+
+  resolve: {
+    extensions: [
+      '.js',
+      '.ts',
+      '.tsx'
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'ts-loader' }
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+
+  optimization: {
+    namedModules: true,
+    splitChunks: {
+      chunks: 'async',
+      cacheGroups: {
+        app: {
+          reuseExistingChunk: true,
+        },
+        vendor: {
+          test: /node_modules/,
+          chunks: 'all',
+          name: 'vendor'
+        }
+      }
+    }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
